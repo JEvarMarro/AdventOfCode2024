d6498541b1b77c4a959c3632867ef9076d871323
Evaristo Marro <evaristo@wyeworks.com>
2024-12-02 22:27:50 -0300

    Fixes on indentation

--- day 1/score_calculator.rb
+++ day 1/score_calculator.rb
@@ -1,9 +1,9 @@
 require_relative 'file_reader'
 
 class ScoreCalculator
-   attr_reader :result
-   def initialize
-      @result = 0
-      calculate
-   end
+  attr_reader :result
+  def initialize
+    @result = 0
+    calculate
+  end
 end
